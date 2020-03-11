e39067d5a6945277e1a41ec12ba62cac11f36e30
dgujs <jinsoo8457@dongguk.edu>
2020-03-11 20:09:11 +0900

    Statenull_add

--- src/App.js
+++ src/App.js
@@ -1,78 +1,109 @@
 import React, { Component, Fragment } from 'react';
 import { createPortal } from 'react-dom';
 
-const BoundaryHOC = ProtectedComponent =>
-  class Boundary extends Component {
-    state = {
-      hasError: false
-    }
-    componentDidCatch = () => {
-      this.setState({
-        hasError: true
-      });
-    };
+// const BoundaryHOC = ProtectedComponent =>
+//   class Boundary extends Component {
+//     state = {
+//       hasError: false
+//     }
+//     componentDidCatch = () => {
+//       this.setState({
+//         hasError: true
+//       });
+//     };
 
-    render() {
-      const { hasError } = this.state;
+//     render() {
+//       const { hasError } = this.state;
 
-      if (hasError) return <ErrorFallBack />
-      else return <ProtectedComponent />
+//       if (hasError) return <ErrorFallBack />
+//       else return <ProtectedComponent />
+//     }
+//   }
+
+// class ErrorMaker extends Component {
+//   state = {
+//     friends: ["jinsoo", "binary"]
+//   }
+//   componentDidMount = () => {
+//     setTimeout(() => {
+//       this.setState({
+//         friends: undefined
+//       })
+//     }, 2000);
+//   };
+//   render() {
+//     const { friends } = this.state;
+//     return friends.map(friend => `${friend}`);
+//   }
+// }
+
+// const PErrorMaker =  BoundaryHOC(ErrorMaker);
+
+// class Portals extends Component {
+//   render() {
+//     return createPortal(
+//       <Message />, document.getElementById("touchme")
+//     );
+//   }
+// }
+
+// const PPortals =  BoundaryHOC(Portals);
+
+// const Message = () => "Just touch me";
+
+// class ReturnTypes extends Component {
+//   render() {
+//     return "HI"
+//   }
+// }
+
+// const PReturnTypes =  BoundaryHOC(ReturnTypes);
+
+// const ErrorFallBack = () => "something wrong"
+
+const MAX_PIZZAS = 20;
+
+const eatPizza = (state, props) => {
+  const { pizzas } = state;
+
+  if (pizzas < MAX_PIZZAS) {
+    return {
+      pizzas: pizzas + 1
     }
   }
+  else {
+    return null;
+  }
+}
 
-class ErrorMaker extends Component {
+class Contolled extends Component {
   state = {
-    friends: ["jinsoo", "binary"]
+    pizzas: 10
   }
-  componentDidMount = () => {
-    setTimeout(() => {
-      this.setState({
-        friends: undefined
-      })
-    }, 2000);
-  };
   render() {
-    const { friends } = this.state;
-    return friends.map(friend => `${friend}`);
+    const { pizzas } = this.state;
+    return (<button onClick={this._handleClick}>
+      {`I have eaten ${pizzas} ${pizzas === 1 ? "pizza" : "pizzas"}`}
+    </button>);
+  }
+
+  _handleClick = () => {
+    this.setState(eatPizza);
   }
 }
-
-const PErrorMaker =  BoundaryHOC(ErrorMaker);
-
-class Portals extends Component {
-  render() {
-    return createPortal(
-      <Message />, document.getElementById("touchme")
-    );
-  }
-}
-
-const PPortals =  BoundaryHOC(Portals);
-
-const Message = () => "Just touch me";
-
-class ReturnTypes extends Component {
-  render() {
-    return "HI"
-  }
-}
-
-const PReturnTypes =  BoundaryHOC(ReturnTypes);
-
-const ErrorFallBack = () => "something wrong"
 
 class App extends Component {
 
   render() {
     return (
-      <>
-        <PReturnTypes />
+      <Contolled>
+        {/* <PReturnTypes />
         <PPortals />
-        <PErrorMaker />
-      </>
+        <PErrorMaker /> */}
+      </Contolled>
     );
   }
 
 }
 
-export default BoundaryHOC(App);
+export default App;
